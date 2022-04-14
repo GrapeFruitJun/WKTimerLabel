778293ca1b06f68801369fae1cbe4b735935f25a
吴凯 <wukai90s@gmail.com>
2022-04-14 22:32:45 +0800

    first commit

---
+++ WKTimerLabel.podspec
@@ -0,0 +1,17 @@
+Pod::Spec.new do |s|
+  s.name         = "WKTimerLabel" 
+  s.version      = "1.0.0"        
+  s.license      = { :type => 'MIT', :file => 'LICENSE' }
+  s.summary      = "WKTimerLabel"
+
+  s.homepage     = "https://github.com/GrapeFruitJun/WKTimerLabel.git"
+  s.source       = { :git => "https://github.com/GrapeFruitJun/WKTimerLabel.git", :tag => "#{s.version}" }
+  s.source_files = "WKTimerLabel/**/*" 
+  s.swift_version = '5'
+  s.platform     = :ios, "11.0" 
+  s.frameworks   = "UIKit", "Foundation" 
+  
+  # User
+  s.author       = { "Justin" => "wukai90s@gmail.com" } 
+
+end
---
+++ WKTimerLabel/WKTimerLabel.swift
@@ -0,0 +1,369 @@
+//
+//  WKTimerLabel.swift
+//  WKTimerLabel
+//
+//  Created by Justin on 2022/4/14.
+//
+
+import Foundation
+import UIKit
+
+public enum WKTimerLabelType: Int {
+    case stopwatch
+    case timer
+}
+
+public protocol WKTimerLabelDelegate {
+    
+    func timerLabel(_ timerLabel: WKTimerLabel, finishedCountDownTimerWith countTime: TimeInterval)
+    func timerLabel(_ timerLabel: WKTimerLabel, countingTo: TimeInterval, timerType: WKTimerLabelType)
+    func timerLabel(_ timerLabel: WKTimerLabel, customTextToDisplayAt time: TimeInterval) -> String?
+    func timerLabel(_ timerLabel: WKTimerLabel, customAttributedTextToDisplayAt time: TimeInterval) -> NSAttributedString?
+}
+
+public class WKTimerLabel:UILabel {
+    
+    private let kHourFormatReplace = "!!!*"
+    private let kDefaultFireIntervalNormal = 0.1
+    private let kDefaultFireIntervalHighUse = 0.01
+    
+    private var timeUserValue: TimeInterval = 0
+    private var startCountDate: Date? = nil
+    private var pausedTime: Date?
+    private var date1970 = Date(timeIntervalSince1970: 0)
+    
+    private var timeToCountOff = Date()
+    
+    private var timer: Timer?
+    lazy private var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_GB")
+        formatter.timeZone = TimeZone(identifier: "GMT") //timeZoneWithName
+        formatter.dateFormat = self.timeFormat
+        return formatter
+    }()
+    
+    private(set) var endedBlock: ((TimeInterval) -> Void)?
+    
+    var delegate: WKTimerLabelDelegate?
+    
+    var timeFormat = "HH:mm:ss" {
+        didSet {
+            self.dateFormatter.dateFormat = timeFormat
+            self.updateLabel()
+        }
+    }
+    
+    weak var timeLabel: UILabel?
+    
+    var textRange: Range<String.Index>?
+    
+    var attributesForTextInRange: [NSAttributedString.Key: Any]?
+    
+    var timerType: WKTimerLabelType = .timer {
+        didSet {
+            self.updateLabel()
+        }
+    }
+    
+    private(set) var counting: Bool = false
+    var resetTimerAfterFinish: Bool = false
+    
+    var shouldCountBeyondHHLimit: Bool = false {
+        didSet {
+            self.updateLabel()
+        }
+    }
+    
+    init(frame: CGRect = .zero, label: UILabel? = nil, timerType: WKTimerLabelType = WKTimerLabelType.timer) {
+        
+        super.init(frame: frame)
+        if label == nil {
+            self.timeLabel = self
+        } else {
+            self.timeLabel = label
+        }
+        self.timerType = timerType
+        self.updateLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        
+        super.init(coder: coder)
+    }
+    
+    public override func awakeFromNib() {
+        
+        super.awakeFromNib()
+        self.timeLabel = self
+    }
+    
+    func start() {
+        //    #if NS_BLOCKS_AVAILABLE
+        //    -(void)startWithEndingBlock:(void(^)(NSTimeInterval countTime))end;
+        //    use it if you are not going to use delegate
+        //    #endif
+        timer?.invalidate()
+        timer = nil
+        if timeFormat.contains("SS") {
+            timer = Timer(timeInterval: kDefaultFireIntervalHighUse, target: self, selector: #selector(updateLabel), userInfo: nil, repeats: true)
+        } else {
+            timer = Timer(timeInterval: kDefaultFireIntervalNormal, target: self, selector: #selector(updateLabel), userInfo: nil, repeats: true)
+        }
+        if let validTimer = timer {
+            RunLoop.current.add(validTimer, forMode: .common)
+        }
+        
+        if startCountDate == nil {
+            startCountDate = Date()
+            if self.timerType == .stopwatch && timeUserValue >= 0 {
+                startCountDate = startCountDate?.addingTimeInterval(-1 * timeUserValue)
+            }
+        }
+        if let validPausedTime = pausedTime,
+           let validStartCountDate = startCountDate {
+            let countedTime = validPausedTime.timeIntervalSince(validStartCountDate)
+            startCountDate = Date().addingTimeInterval(-1 * countedTime)
+            pausedTime = nil
+        }
+        counting = true
+        timer?.fire()
+    }
+    
+    func start(with endingBlock: ((TimeInterval) -> Void)?) {
+        
+        self.endedBlock = endingBlock
+        self.start()
+    }
+    
+    func pause() {
+        
+        if counting {
+            timer?.invalidate()
+            timer = nil
+            counting = false
+            pausedTime = Date()
+        }
+    }
+    
+    func reset() {
+        
+        pausedTime = nil
+        timeUserValue = (self.timerType == .stopwatch) ? 0 : timeUserValue
+        startCountDate = self.counting ? Date() : nil
+        self.updateLabel()
+    }
+    
+    func addTimeCounted(by time: TimeInterval) {
+        
+        if timerType == .timer {
+            self.setCountDown(time: time + timeUserValue)
+        } else if timerType == .stopwatch,
+                  let newStartDate = startCountDate?.addingTimeInterval(-1 * time) {
+            if Date().timeIntervalSince(newStartDate) <= 0 {
+                //prevent less than 0
+                startCountDate = Date()
+            } else {
+                startCountDate = newStartDate
+            }
+        }
+        self.updateLabel()
+    }
+    
+    func getTimeCounted() -> TimeInterval {
+        
+        guard let validStartCountDate = startCountDate else {
+            return 0
+        }
+        var countedTime = Date().timeIntervalSince(validStartCountDate)
+        if let validPausedTime = pausedTime {
+            let pauseCountedTime = Date().timeIntervalSince(validPausedTime)
+            countedTime -= pauseCountedTime
+        }
+        return countedTime
+    }
+    
+    func getTimeRemaining() -> TimeInterval {
+        
+        if timerType == .timer {
+            return (timeUserValue - self.getTimeCounted())
+        }
+        return 0
+    }
+    
+    func getCountDownTime() -> TimeInterval {
+        
+        if timerType == .timer {
+            return timeUserValue
+        }
+        return 0
+    }
+    
+    public override func removeFromSuperview() {
+        
+        timer?.invalidate()
+        timer = nil
+        super.removeFromSuperview()
+    }
+    
+    // MARK: - Getter and Setter Method
+    
+    func setStopWatch(time: TimeInterval) {
+        
+        self.timeUserValue = (time < 0) ? 0 : time
+        if timeUserValue > 0 {
+            self.startCountDate = Date().addingTimeInterval(-1 * timeUserValue)
+            self.pausedTime = Date()
+            self.updateLabel()
+        }
+        
+    }
+    
+    func setCountDown(time: TimeInterval) {
+        
+        self.timeUserValue = (time < 0) ? 0 : time
+        timeToCountOff = date1970.addingTimeInterval(timeUserValue)
+        self.updateLabel()
+    }
+    
+    func setCountDownTo(date: Date) {
+        
+        let timeLeft = date.timeIntervalSince(Date())
+        if timeLeft > 0 {
+            self.timeUserValue = timeLeft
+            self.timeToCountOff = date1970.addingTimeInterval(timeLeft)
+        } else {
+            self.timeUserValue = 0
+            self.timeToCountOff = date1970.addingTimeInterval(0)
+        }
+        self.updateLabel()
+        
+    }
+    
+    deinit {
+        timer?.invalidate()
+    }
+    
+}
+
+extension WKTimerLabel {
+    
+    @objc
+    func updateLabel() {
+        
+        var timeDiff: TimeInterval = 0
+        if let startDate = startCountDate {
+            timeDiff = Date().timeIntervalSince(startDate)
+        }
+        var timeToShow = Date()
+        var timerEnded = false
+        
+        /***WKTimerLabelTypeStopWatch Logic***/
+        if timerType == .stopwatch {
+            
+            if counting {
+                timeToShow = date1970.addingTimeInterval(timeDiff)
+            } else {
+                //  timeToShow = date1970.addingTimeInterval(!startCountDate)?0:timeDiff)
+                if let _ = startCountDate {
+                    timeToShow = date1970.addingTimeInterval(timeDiff)
+                } else {
+                    timeToShow = date1970.addingTimeInterval(0)
+                }
+            }
+            delegate?.timerLabel(self, countingTo: timeDiff, timerType: timerType)
+            
+        } else {
+            /***WKTimerLabelTypeTimer Logic***/
+            if counting {
+                let timeLeft = timeUserValue - timeDiff
+                delegate?.timerLabel(self, countingTo: timeLeft, timerType: timerType)
+                if timeDiff >= timeUserValue {
+                    self.pause()
+                    timeToShow = date1970.addingTimeInterval(0)
+                    startCountDate = nil
+                    timerEnded = true
+                } else {
+                    timeToShow = timeToCountOff.addingTimeInterval(timeDiff * -1) //added 0.999 to make it actually counting the whole first second
+                }
+            } else {
+                timeToShow = timeToCountOff
+            }
+            
+        }
+        //setting text value
+        
+        let atTime = timerType == .stopwatch ? timeDiff : ((timeUserValue - timeDiff) < 0 ? 0 : (timeUserValue - timeDiff))
+        if delegate?.timerLabel(self, customTextToDisplayAt: atTime) != nil {
+            
+            if let customText = delegate?.timerLabel(self, customTextToDisplayAt: atTime), !customText.isEmpty {
+                self.timeLabel?.text = customText
+            } else {
+                self.timeLabel?.text = self.dateFormatter.string(from: timeToShow)
+            }
+            
+        } else if delegate?.timerLabel(self, customAttributedTextToDisplayAt: atTime) != nil {
+            
+            let atTime = timerType == .stopwatch ? timeDiff : ((timeUserValue - timeDiff) < 0 ? 0 : (timeUserValue - timeDiff))
+            if let customText = delegate?.timerLabel(self, customAttributedTextToDisplayAt: atTime),
+               !customText.string.isEmpty {
+                self.timeLabel?.attributedText = customText
+            } else {
+                self.timeLabel?.text = self.dateFormatter.string(from: timeToShow)
+            }
+            
+        } else {
+            
+            if shouldCountBeyondHHLimit {
+                
+                var beyondFormat = String(timeFormat)
+                beyondFormat = beyondFormat.replacingOccurrences(of: "HH", with: kHourFormatReplace)
+                beyondFormat = beyondFormat.replacingOccurrences(of: "H", with: kHourFormatReplace)
+                self.dateFormatter.dateFormat = beyondFormat
+                let hours = (timerType == .stopwatch) ? self.getTimeCounted() / 3600 : self.getTimeRemaining() / 3600
+                let formattedDate = self.dateFormatter.string(from: timeToShow)
+                let beyondedDate = formattedDate.replacingOccurrences(of: kHourFormatReplace, with: String(format: "%02.0f", hours))
+                self.timeLabel?.text = beyondedDate
+                self.dateFormatter.dateFormat = timeFormat
+                
+            } else {
+                
+                if let validRange = textRange, !validRange.isEmpty {
+                    
+                    if let attributes = self.attributesForTextInRange,!attributes.isEmpty {
+                        
+                        let attrTextInRange = NSAttributedString(string: self.dateFormatter.string(from: timeToShow), attributes: attributes)
+                        let attributedString = NSMutableAttributedString(string: self.text ?? "")
+                        let nsRange = NSRange(validRange, in: self.text ?? "")
+                        // Replace content in range with the new content
+                        attributedString.replaceCharacters(in: nsRange, with: attrTextInRange)
+                        self.timeLabel?.attributedText = attributedString
+                        
+                    } else {
+                        
+                        let labelText = self.text?.replacingCharacters(in: validRange, with: self.dateFormatter.string(from: timeToShow))
+                        self.timeLabel?.text = labelText
+                        
+                    }
+                    
+                } else {
+                    
+                    self.timeLabel?.text = self.dateFormatter.string(from: timeToShow)
+                }
+            }
+        }
+        
+        if timerEnded {
+            
+            delegate?.timerLabel(self, finishedCountDownTimerWith: timeUserValue)
+            //if NS_BLOCKS_AVAILABLE
+            self.endedBlock?(timeUserValue)
+            if resetTimerAfterFinish {
+                self.reset()
+            }
+        }
+        
+    }
+    
+}
+
